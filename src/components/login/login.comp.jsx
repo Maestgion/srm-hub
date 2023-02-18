7e4173b5fdba21b117affc97e32031d8d06af932
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-02-18 08:08:22 +0530

    login signup setup

--- src/components/login/login.comp.jsx
+++ src/components/login/login.comp.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../../state';
+import { useNavigate } from "react-router";
+
+import Cookies from 'js-cookie'
+import { Link } from "react-router-dom";
+
 
 function LoginComp() {
-  return <div className="text-3xl font-medium">LoginComp</div>;
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { setUser } = bindActionCreators(actionCreators, dispatch);
+  const handleLogin = () => {
+    const data = {
+      email,
+      password,
+    }
+    console.log(data)
+    setUser(data)
+    // axios.post(`${API_URI}/auth/login`, data)
+    //   .then(res => {
+    //     console.log(res.data)
+    //     setUser(res.data.data)
+    //     Cookies.set('uid', res.data.data.id)
+    //     toast.success(`Login Successful`)
+    //     navigate('/')
+    //   }).catch(err => {
+    //     console.log(err)
+    //     toast.error(`Login Failed`)
+    //   })
+  }
+
+  return <div className="">
+    <div className="">
+      <h2>
+        SRM HUB Mobile Interface coming soon... <br></br>
+        To use the application open it in desktop or tablet.
+      </h2>
+    </div>
+    <div className="">
+      <div>
+        <div className="">
+          {/* <img src={Logo1} className="" /> */}
+        </div>
+        <h1>
+          Welcome to the<br></br>SRM HUB
+        </h1>
+      </div>
+    </div>
+    <form className="" onSubmit={(e) => { e.preventDefault(); handleLogin() }} >
+      <h1>Login</h1>
+      <div className="">
+        <p>Enter the credentials to access the portal</p>
+      </div>
+      <div className="">
+        <div className="">
+          <label>Email</label>
+          <input type="email" placeholder="Ex. John Doe" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="">
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className="">
+          <Link to="/forgotpassword">Forgot Password?</Link><br></br>
+          <Link to="/signup">Request Access?</Link>
+        </div>
+      </div>
+      <div className="">
+        <button type={'submit'}>Login</button>
+      </div>
+    </form>
+  </div>
 }
 
 export default LoginComp;
--- src/components/signup/signup.comp.jsx
+++ src/components/signup/signup.comp.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignupComp() {
-  return <div>SignupComp</div>;
+  const [email, setEmail] = useState(null)
+  const [department, setDepartment] = useState(null)
+  const [password, setPassword] = useState("")
+
+
+  return <div className="">
+    <div className="">
+      <h2>
+        SRM HUB Mobile Interface coming soon... <br></br>
+        To use the application open it in desktop or tablet.
+      </h2>
+    </div>
+    <div className="">
+      <div>
+        <div className="">
+          {/* <img src={Logo1} className="" /> */}
+        </div>
+        <h1>
+          Welcome to the<br></br>SRM HUB
+        </h1>
+      </div>
+      <div className=""> &copy; Copyrights All Rights Reserved </div>
+    </div>
+    <div className="">
+      <h1>Sign Up</h1>
+      <div className="">
+        <p>Enter the following details to request access in the portal</p>
+      </div>
+      <div className="">
+        <div className="">
+          <div className="">
+            <label>Email</label>
+            <input type="email" placeholder="Ex. abc@srmist.edu.in" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="">
+            <label>Department</label>
+            <input type="String" placeholder="Ex. CTech, CSBS, etc" onChange={(e) => setDepartment(e.target.value)} />
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <Link to="/login">Already have Access? Login</Link>
+      </div>
+      <div className="">
+        <button onClick={() => { handleSignup() }}>Sign Up</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default SignupComp;
